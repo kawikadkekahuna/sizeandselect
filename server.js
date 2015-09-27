b96d92c1b86ddab62873a50fba47925e70f872bd
Judah Lamm <Judah.Lamm@gmail.com>
2015-09-27 18:01:59 -0400

    adding in passport no setup done

--- server.js
+++ server.js
@@ -3,8 +3,44 @@
 var express = require('express');
 var app = express();
 var db = require('./models');
+var User = db.User;
 var bodyParser = require('body-parser');
 var routes = require('./routes');
+
+var session = require('express-session')
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var flash = require('connect-flash');
+var bcrypt = require('bcrypt');
+var csurf = require('csurf');
+var async = require('async');
+var crypto = require('crypto');
+
+
+
+
+
+app.use(express.static('public'));
+// app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+//Need to use a production type middleware for sessions
+//https://github.com/expressjs/session?_ga=1.52528104.1907317942.1442619285
+//https://www.npmjs.com/package/connect-pg-simple
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true
+  }
+}))
+app.use(flash());
+app.use(csurf());
+
+
+
+
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -12,6 +48,42 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
   next();
 });
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+function localStrategy (email, password, done) {
+  process.nextTick(function () {
+      User.findOne({
+        where: { email: email }
+      }).then(function(user) {
+      if (!user) {
+        return done(null, false, { message: 'Incorrect email.' });
+      }
+      if (!bcrypt.compareSync(password, user.password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    }).catch(function (err) {
+        return done(err, null);
+        throw err;
+      });
+  });
+}
+
+passport.use(new LocalStrategy(localStrategy));
+
+
 
 app.use('/api', routes);
 
