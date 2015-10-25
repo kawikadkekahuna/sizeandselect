f38977a7c7c677185e2b4a633ded360f8333be7c
Judah Lamm <Judah.Lamm@gmail.com>
2015-10-25 13:02:18 -0600

    add liquid media model and bill sql setup

--- models/MockSQL/billSQL.sql
+++ models/MockSQL/billSQL.sql
@@ -1,13 +1,16 @@
-INSERT INTO states VALUES (DEFAULT,'Wyoming','WY');
-
+-- name
 INSERT INTO pressure_units VALUES (DEFAULT, '');
 
+-- name
 INSERT INTO temperature_units VALUES (DEFAULT, '');
 
+-- name
 INSERT INTO viscosity_units VALUES (DEFAULT, '');
 
+-- name
 INSERT INTO flow_capacity_units VALUES (DEFAULT, '');
 
+-- name
 INSERT INTO devices VALUES (DEFAULT, 'Relief Valves');
 INSERT INTO devices VALUES (DEFAULT, 'Tank Vents');
 INSERT INTO devices VALUES (DEFAULT, 'Regulators');
@@ -16,3 +19,9 @@ INSERT INTO devices VALUES (DEFAULT, 'Control Valves');
 INSERT INTO devices VALUES (DEFAULT, 'Liquid Level');
 INSERT INTO devices VALUES (DEFAULT, 'Pressure | Temperature Transmitters');
 INSERT INTO devices VALUES (DEFAULT, 'Instrumentation Valves');
+
+-- name, specific gravity, viscosity
+INSERT INTO liquid_medias VALUES (DEFAULT, 'water', '1', '2');
+
+-- name
+
