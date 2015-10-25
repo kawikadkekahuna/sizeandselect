aac33b9baab4d498d506dfe4a4947412243a3ad4
Judah Lamm <Judah.Lamm@gmail.com>
2015-10-25 12:58:26 -0600

    update bill sql setup file and created device model

--- models/MockSQL/billSQL.sql
+++ models/MockSQL/billSQL.sql
@@ -1,20 +1,18 @@
-INSERT INTO pressure_units (name) VALUES
-(""),
-(""),
-(""),
-("");
-INSERT INTO temperature_units (name) VALUES
-(""),
-(""),
-(""),
-("");
-INSERT INTO viscosity_units (name) VALUES
-(""),
-(""),
-(""),
-("");
-INSERT INTO flow_capacity_units (name) VALUES
-(""),
-(""),
-(""),
-("");
+INSERT INTO states VALUES (DEFAULT,'Wyoming','WY');
+
+INSERT INTO pressure_units VALUES (DEFAULT, '');
+
+INSERT INTO temperature_units VALUES (DEFAULT, '');
+
+INSERT INTO viscosity_units VALUES (DEFAULT, '');
+
+INSERT INTO flow_capacity_units VALUES (DEFAULT, '');
+
+INSERT INTO devices VALUES (DEFAULT, 'Relief Valves');
+INSERT INTO devices VALUES (DEFAULT, 'Tank Vents');
+INSERT INTO devices VALUES (DEFAULT, 'Regulators');
+INSERT INTO devices VALUES (DEFAULT, 'Flame Arrestors');
+INSERT INTO devices VALUES (DEFAULT, 'Control Valves');
+INSERT INTO devices VALUES (DEFAULT, 'Liquid Level');
+INSERT INTO devices VALUES (DEFAULT, 'Pressure | Temperature Transmitters');
+INSERT INTO devices VALUES (DEFAULT, 'Instrumentation Valves');
