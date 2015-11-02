171efb0540e6ad8fa5772d329cda81d711b2df81
Judah Lamm <Judah.Lamm@gmail.com>
2015-11-01 21:04:30 -0600

    adds assocaiton between users and account types

--- models/User.js
+++ models/User.js
@@ -26,25 +26,7 @@ module.exports = function (sequelize, DataTypes) {
     underscored: true,
     tableName: "users",
     classMethods: {
-      /**
-       * [associate]
-       * @description {
-       * Associate function used to create inner joins between tables.
-       * Inner join linked between 'account_types'.
-       * Inner join linked between 'projects'.
-       * Inner join linked between 'user_accesses'.
-       * Inner join linked between 'message'.
-       * Inner join linked between 'message_rooms'.
-       * Relationship association: hasOne('account_types')
-       * Relationship association: hasMany('projects')
-       * Relationship association: belongsTo('user_accesses')
-       * Relationship association: hasMany('messages')
-       * Relationship association: hasMany('message_rooms')
-       * }
-       */
-      // associate: function(models) {
-
-      // }
+      User.belongsTo(models.AccountType, {foreignKey: 'account_type_id'});
     }
   });
 
