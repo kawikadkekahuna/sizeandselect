5a32f95b7e90bae5568c1b856cbb552d9628558f
Judah Lamm <Judah.Lamm@gmail.com>
2015-12-13 23:38:08 -0600

    cleanup valve selection model - remove all hard coded FK as they are the same

--- models/ValveSelection.js
+++ models/ValveSelection.js
@@ -7,18 +7,17 @@ module.exports = function (sequelize, DataTypes) {
       autoIncrement: true
     },
   }, {
-    underscored: true,
     tableName: "valve_selections",
     classMethods: {
       associate: function(models) {
-        ValveSelection.belongsTo(models.OrificeSize, {foreignKey: 'orifice_size_id'});
-        ValveSelection.belongsTo(models.ConnectionSize, {foreignKey: 'connection_size_id'});
-        ValveSelection.belongsTo(models.InletFlange, {foreignKey: 'inlet_flange_id'});
-        ValveSelection.belongsTo(models.FlangeFacing, {foreignKey: 'flange_facing_id'});
-        ValveSelection.belongsTo(models.TrimType, {foreignKey: 'trim_type_id'});
-        ValveSelection.belongsTo(models.SeatType, {foreignKey: 'seat_type_id'});
-        ValveSelection.belongsTo(models.MaterialVariable, {foreignKey: 'material_variable_id'});
-        ValveSelection.belongsTo(models.TagCalculation, {foreignKey: 'tag_calculation_id'});
+        ValveSelection.belongsTo(models.OrificeSize);
+        ValveSelection.belongsTo(models.ConnectionSize);
+        ValveSelection.belongsTo(models.InletFlange);
+        ValveSelection.belongsTo(models.FlangeFacing);
+        ValveSelection.belongsTo(models.TrimType);
+        ValveSelection.belongsTo(models.SeatType);
+        ValveSelection.belongsTo(models.MaterialVariable);
+        ValveSelection.belongsTo(models.TagCalculation);
       }
     }
   });
