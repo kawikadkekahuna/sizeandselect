db1a4c6410215a9b75fdffd45926dc5f7f2e9aff
Judah Lamm <Judah.Lamm@gmail.com>
2016-02-20 21:50:19 -0600

    Add hoem page information

--- public/app/user/controllers/HomeController.js
+++ public/app/user/controllers/HomeController.js
@@ -1,5 +1,13 @@
 angular.module('app')
 
 .controller('HomeController', function ($scope, $state, $stateParams, $rootScope, $sessionStorage, $location, $localStorage, ProjectService, ModalFactory, HelperFactory) {
-	
+
+
+
+    <script type="text/javascript">
+        var clock = $('.home__time-counter').FlipClock({
+        // ... your options here
+        });
+        clock.setTime(3600);
+    </script>
 });
