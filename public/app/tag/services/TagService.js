a33b4e099cb58c76b10460b4d016400716d2c988
Judah Lamm <Judah.Lamm@gmail.com>
2016-03-27 19:17:42 -0500

    Refactor tag service for indentation and scope

--- public/app/tag/services/TagService.js
+++ public/app/tag/services/TagService.js
@@ -1,32 +1,35 @@
-angular.module('app')
+(function () {
+    "use strict";
 
-.service('TagService',['$http','$state',TagService])
+    function TagService ($http, $state) {
 
-function TagService ($http, $state) {
+        this.addTag = function (tagData) {
+            return $http.post(SERVER + '/api/tags', tagData);
+        };
 
-  this.addTag = function (tagData){
-    return $http.post(SERVER + '/api/tags', tagData);
-  };
+        this.getTags = function (projectId) {
+            return $http.get(SERVER + '/api/tags', {params: {projectId: projectId}});
+        };
 
-  this.getTags = function (projectId){
-    return $http.get(SERVER + '/api/tags', { params:{projectId: projectId}});
-  };
+        this.getTagById = function (tagId) {
+            return $http.get(SERVER + '/api/tags/' + tagId);
+        };
 
-  this.getTagById = function (tagId){
-    return $http.get(SERVER + '/api/tags/' + tagId);
-  };
+        this.createTagSheet = function (tagData) {
+            return $http.post(SERVER + '/api/tags/tag-sheet', tagData);
+        };
 
-  this.createTagSheet = function(tagData){
-    return $http.post(SERVER + '/api/tags/tag-sheet', tagData)
-  };
+        this.defaultValues = function () {
+            var config = {
+                ATM_PRESSURE: 14.696,
+                COMPRESSIBILITY: 1,
+                RUPTURE_DISC: 1
+            };
 
-  this.defaultValues = function (){
-    var config = {
-      ATM_PRESSURE: 14.696,
-      COMPRESSIBILITY: 1,
-      RUPTURE_DISC: 1
-    };
-    return config;
-  };
+            return config;
+        };
+    }
 
-};
+    angular.module('app')
+        .service('TagService', ['$http', '$state', TagService]);
+}());
